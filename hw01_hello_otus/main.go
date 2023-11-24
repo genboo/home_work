23e77bab6ba9372f2c830f1040a46a08ac7e2162
genboo <gen.masj@gmail.com>
2023-11-25 00:07:59 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	_, err := fmt.Fprintln(os.Stdout, reverse.String("Hello, OTUS!"))
+	if err != nil {
+		return
+	}
 }
