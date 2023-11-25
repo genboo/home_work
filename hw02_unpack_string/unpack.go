f776e93ef07e023a14008f69ae020b4869a86907
genboo <gen.masj@gmail.com>
2023-11-26 01:00:42 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	// пустое значение возвращает пустое значение
+	if str == "" {
+		return "", nil
+	}
+
+	runes := []rune(str)
+	// не должно начинаться с цифры
+	if _, err := strconv.Atoi(string(runes[0])); err == nil {
+		return "", ErrInvalidString
+	}
+	// недопустимы числа
+	reg := regexp.MustCompile(`[0-9]{2,}`)
+	if reg.MatchString(str) {
+		return "", ErrInvalidString
+	}
+
+	result := strings.Builder{}
+	var r rune = 0
+	for i := range runes {
+		rn := runes[i]
+		if val, err := strconv.Atoi(string(rn)); err == nil {
+			result.WriteString(strings.Repeat(string(r), val))
+			r = 0
+		} else {
+			if r != 0 {
+				result.WriteRune(r)
+			}
+			r = rn
+		}
+	}
+	if r != 0 {
+		result.WriteRune(r)
+	}
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,15 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "try2[1]0", expected: "tryy["},
+		{input: "n", expected: "n"},
+		{input: "q4", expected: "qqqq"},
+		{input: "-1", expected: "-"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		//{input: `qwe\4\5`, expected: `qwe45`},
+		//{input: `qwe\45`, expected: `qwe44444`},
+		//{input: `qwe\\5`, expected: `qwe\\\\\`},
+		//{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +38,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "1", "tt56"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
