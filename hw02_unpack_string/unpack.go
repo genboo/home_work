29346d303bfe1a9acc823e8794c071efb32f4f63
genboo <gen.masj@gmail.com>
2023-11-26 01:06:26 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,24 +10,24 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	// пустое значение возвращает пустое значение
+	//пустое значение возвращает пустое значение
 	if str == "" {
 		return "", nil
 	}
 
 	runes := []rune(str)
-	// не должно начинаться с цифры
+	//не должно начинаться с цифры
 	if _, err := strconv.Atoi(string(runes[0])); err == nil {
 		return "", ErrInvalidString
 	}
-	// недопустимы числа
+	//недопустимы числа
 	reg := regexp.MustCompile(`[0-9]{2,}`)
 	if reg.MatchString(str) {
 		return "", ErrInvalidString
 	}
 
 	result := strings.Builder{}
-	var r rune = 0
+	var r rune
 	for i := range runes {
 		rn := runes[i]
 		if val, err := strconv.Atoi(string(rn)); err == nil {
